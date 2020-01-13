af46da8c28a53d867272c13a59981e08eac2960b
Judson Belmont <belmoj01@li03c04.chimera.hpc.mssm.edu>
2020-01-13 13:18:09 -0500

    Calculate enrichment stats & generate plot all within same script + cosmetic fixes

--- analysis/qtltools_summaryplot.R
+++ analysis/qtltools_summaryplot.R
@@ -1,17 +1,43 @@
 library(ggplot2)
 
-df = data.frame(region=c('introns_only','exons_only','coding_exons_only','500bp_downstream_only','500bp_upstream_only','genes_only','3UTR','5UTR'),OR=c(0.9592288,0.8888918,0.5867461,1.05753,1.267125,0.96544,0.963563,1.1042),pval=c(0.009148,0.0008403,2.2e-16,0.3359,3.174e-05,0.02153,0.4692,0.1181),stringsAsFactors=FALSE)
+res = list('introns_only'='/sc/orga/projects/EPIASD/splicingQTL/intermediate_files/fqtl_output/20genoPCs_nogeno/deduped_mincovars_40HCPs/qtlTools_results/gencodeV19intronsOnly_qtltools_enrich.txt',
+  'exons_only'='/sc/orga/projects/EPIASD/splicingQTL/intermediate_files/fqtl_output/20genoPCs_nogeno/deduped_mincovars_40HCPs/qtlTools_results/gencodeV19exonsOnly_qtltools_enrich.txt',
+  'coding_exons_only'='/sc/orga/projects/EPIASD/splicingQTL/intermediate_files/fqtl_output/20genoPCs_nogeno/deduped_mincovars_40HCPs/qtlTools_results/gencodeV19codingexonsOnly_qtltools_enrich.txt',
+  '2000bp_downstream_only'='/sc/orga/projects/EPIASD/splicingQTL/intermediate_files/fqtl_output/20genoPCs_nogeno/deduped_mincovars_40HCPs/qtlTools_results/gencodeV192000downstreamOnly_qtltools_enrich.txt',
+  '1000bp_downstream_only'='/sc/orga/projects/EPIASD/splicingQTL/intermediate_files/fqtl_output/20genoPCs_nogeno/deduped_mincovars_40HCPs/qtlTools_results/gencodeV191000downstreamOnly_qtltools_enrich.txt',
+  '750bp_downstream_only'='/sc/orga/projects/EPIASD/splicingQTL/intermediate_files/fqtl_output/20genoPCs_nogeno/deduped_mincovars_40HCPs/qtlTools_results/gencodeV19750downstreamOnly_qtltools_enrich.txt',
+  '500bp_downstream_only'='/sc/orga/projects/EPIASD/splicingQTL/intermediate_files/fqtl_output/20genoPCs_nogeno/deduped_mincovars_40HCPs/qtlTools_results/gencodeV19500downstreamOnly_qtltools_enrich.txt',
+  '250bp_downstream_only'='/sc/orga/projects/EPIASD/splicingQTL/intermediate_files/fqtl_output/20genoPCs_nogeno/deduped_mincovars_40HCPs/qtlTools_results/gencodeV19250downstreamOnly_qtltools_enrich.txt',
+  '2000bp_upstream_only'='/sc/orga/projects/EPIASD/splicingQTL/intermediate_files/fqtl_output/20genoPCs_nogeno/deduped_mincovars_40HCPs/qtlTools_results/gencodeV192000upstreamOnly_qtltools_enrich.txt',
+  '1000bp_upstream_only'='/sc/orga/projects/EPIASD/splicingQTL/intermediate_files/fqtl_output/20genoPCs_nogeno/deduped_mincovars_40HCPs/qtlTools_results/gencodeV191000upstreamOnly_qtltools_enrich.txt',
+  '750bp_upstream_only'='/sc/orga/projects/EPIASD/splicingQTL/intermediate_files/fqtl_output/20genoPCs_nogeno/deduped_mincovars_40HCPs/qtlTools_results/gencodeV19750upstreamOnly_qtltools_enrich.txt',
+  '500bp_upstream_only'='/sc/orga/projects/EPIASD/splicingQTL/intermediate_files/fqtl_output/20genoPCs_nogeno/deduped_mincovars_40HCPs/qtlTools_results/gencodeV19500upstreamOnly_qtltools_enrich.txt',
+  '250bp_upstream_only'='/sc/orga/projects/EPIASD/splicingQTL/intermediate_files/fqtl_output/20genoPCs_nogeno/deduped_mincovars_40HCPs/qtlTools_results/gencodeV19250upstreamOnly_qtltools_enrich.txt',
+  'genes_only'='/sc/orga/projects/EPIASD/splicingQTL/intermediate_files/fqtl_output/20genoPCs_nogeno/deduped_mincovars_40HCPs/qtlTools_results/gencodeV19genesOnly_qtltools_enrich.txt',
+  '3UTR'='/sc/orga/projects/EPIASD/splicingQTL/intermediate_files/fqtl_output/20genoPCs_nogeno/deduped_mincovars_40HCPs/qtlTools_results/gencodeV193UTRexonsOnly_qtltools_enrich.txt',
+  '5UTR'='/sc/orga/projects/EPIASD/splicingQTL/intermediate_files/fqtl_output/20genoPCs_nogeno/deduped_mincovars_40HCPs/qtlTools_results/gencodeV195UTRexonsOnly_qtltools_enrich.txt')
 
-df$label = round(df$pval,digits=2)
-df$label[which(df$label==0)] = '<.001'
+stats = lapply(names(res),function(x){
+  infile = res[[x]]
+  print(infile)
+  qtlres = read.table(infile, head=FALSE, stringsAsFactors=FALSE)
+  ftestres = fisher.test(matrix(c(qtlres$V1, qtlres$V2, round(qtlres$V3), qtlres$V2), ncol=2))
+  return(data.frame(region=x,pval=ftestres$p.value, OR=ftestres$estimate))
+})
+
+df = do.call(rbind,stats)
+
+df$label = round(df$OR,digits=2)
+df$label[which(df$pval>=.05)] = ''
 
 df$enrichment = 'sQTL enrichment'
 
-p = ggplot(df, aes(region,enrichment, fill=OR)) + 
+p = ggplot(df, aes(region,enrichment, fill=-log10(pval))) + 
   ggtitle('sQTL enrichments by genomic region (GENCODE v19)') +
   geom_tile() +
-  geom_text(aes(label=label), color = 'black', size = 6) + 
-  scale_fill_gradient2(low = 'blue', mid = 'white', high = 'red', midpoint = 1, limits=c(0,2)) +
+  geom_text(aes(label=label), color = 'black', size = 4) + 
+  #scale_fill_gradient2(low = 'blue', mid = 'white', high = 'red', midpoint = 1, limits=c(0,20)) +
+  scale_fill_gradient(low = 'white', high = 'red', limits=c(0,20)) +
   theme(
     axis.title.x = element_blank(),
     axis.title.y = element_blank(),
@@ -32,4 +58,6 @@ p = ggplot(df, aes(region,enrichment, fill=OR)) +
 
 plot(p)
 
-ggsave('qtltools_res_summary.png', plot = p, dpi = 300)
+aspect_ratio = 1.5
+height = 7
+ggsave('qtltools_res_summary.png', plot = p, dpi = 300, height=height, width=height*aspect_ratio)
