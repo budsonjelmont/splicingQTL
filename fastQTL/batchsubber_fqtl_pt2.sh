05bbfd1bea519c6a8f06d9874aa90133b86ecfe8
Judson Belmont <budsonjelmont@gmail.com>
2020-09-29 19:17:32 -0400

    Fixed a typo in fasQTL batchsubber

--- fastQTL/batchsubber_fqtl_pt2.sh
+++ fastQTL/batchsubber_fqtl_pt2.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-baseDir='/sc/arion/projects/EPIASD/splicingQTL/intermediate_files/fqtl_output_wasp/20genoPCs_nogenoInHCP/deduped_mincovars+seqPC9_55HCPs/'
+resDirBase='/sc/arion/projects/EPIASD/splicingQTL/intermediate_files/fqtl_output_wasp/20genoPCs_nogenoInHCP/deduped_mincovars+seqPC9_'
 
-# List the contents of the directory passed as argument 1 and read all files there into an array 
+# Parameters to iterate 
+hcps=($(seq 0 5 100))
 chrs=($(seq 1 1 22))
 
 # Loop through the array and ship off each command to bsub
-for chr in "${chrs[@]}"
+for hcps in "${hcps[@]}"
 do
-  Command="ml selfsched; ml fastqtl; mpirun selfsched < ${baseDir}runFQTL_splitcommand_chr${chr}.sh"
-  echo ${Command}
-  bsub -J fQTL_pt2_chr${chr} -P acc_pintod02b -q premium -n 50 -W 4:00 -R rusage[mem=5000] -o /sc/arion/projects/EPIASD/splicingQTL/lsf_output/fQTL_pt2_norm_perm1000_chr$chr ${Command}
+  resDir=$resDirBase${hcps}HCPs/
+  for chr in "${chrs[@]}"
+  do
+    Command="ml selfsched; ml fastqtl; mpirun selfsched < ${resDir}runFQTL_splitcommand_chr${chr}.sh"
+    echo ${Command}
+    bsub -J fQTL_pt2_chr${chr} -P acc_pintod02b -q premium -n 50 -W 0:50 -R rusage[mem=5000] -o /sc/arion/projects/EPIASD/splicingQTL/lsf_output/fQTL_pt2_norm_perm1000_chr$chr ${Command}
+  done
 done
