1b28aa624f6698754bac4d7cecf93dc8b9cd7de5
Judson Belmont <budsonjelmont@gmail.com>
2020-10-05 18:17:07 -0400

    Estimate ancestry &

--- PCA/viz_1kg_genoPCA.R
+++ PCA/viz_1kg_genoPCA.R
@@ -1,39 +1,3 @@
-multiplot = function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-  require(grid)
-  
-  # Make a list from the ... arguments and plotlist
-  plots = c(list(...), plotlist)
-  
-  numPlots = length(plots)
-  
-  # If layout is NULL, then use 'cols' to determine layout
-  if (is.null(layout)) {
-    # Make the panel
-    # ncol: Number of columns of plots
-    # nrow: Number of rows needed, calculated from # of cols
-    layout = matrix(seq(1, cols * ceiling(numPlots/cols)),
-                     ncol = cols, nrow = ceiling(numPlots/cols))
-  }
-  
-  if (numPlots==1) {
-    print(plots[[1]])
-    
-  } else {
-    # Set up the page
-    grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-    
-    # Make each plot, in the correct location
-    for (i in 1:numPlots) {
-      # Get the i,j matrix positions of the regions that contain this subplot
-      matchidx = as.data.frame(which(layout == i, arr.ind = TRUE))
-      
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-                                      layout.pos.col = matchidx$col))
-    }
-  }
-}
-
 ggbiplot = function(eigenvec,x,y,savepath){
   pcx = paste0('PC',as.character(x))
   pcy = paste0('PC',as.character(y))
@@ -54,7 +18,7 @@ ggbiplot = function(eigenvec,x,y,savepath){
     axis.title.y = element_text(color='black', size = 14),
     axis.text = element_text(color = 'black', size = 11),
     plot.margin=unit(c(t=15,r=17,b=17,l=17),'pt'),
-    panel.border = element_rect(colour = "black", fill=NA, size=2)
+    panel.border = element_rect(colour = 'black', fill=NA, size=2)
   )
   ggsave(paste0(savepath,'ancestryPCA_',pcx,'vs',pcy,'.png'), width=pwidth, plot = g)
   return(g)
@@ -65,7 +29,6 @@ est_ancestry = function(eigenvec,g,x,y,conflevel,savepath){
   pcy = paste0('PC',as.character(y))
 
   for (z in unique(eigenvec['ancestry'][eigenvec['ancestry']!='This study'])){
-     #dat = cbind(eigenvec[,1:20],replace(eigenvec['ancestry'],eigenvec['ancestry']!=z,NA))
      dat = eigenvec[which(eigenvec['ancestry']==z),]
      # Add ellipse to plot to capture region containing x proportion of data points 
      g = g + stat_ellipse(data=dat,method='t',alpha=0.55,level=conflevel,na.rm=T)
@@ -79,6 +42,19 @@ est_ancestry = function(eigenvec,g,x,y,conflevel,savepath){
   #) 
   ggsave(paste0(savepath,'ancestryPCAwithEllipse_',pcx,'vs',pcy,'.png'), width=pwidth, plot = g)
   return(eigenvec) 
+}
+
+# Make loadings heatmap (reqs library ComplexHeatmap)
+plot_loadings = function(eigenvec,eigenval,outpath,basefile){
+ pc_cols = grep('^PC[.]*', colnames(eigenvec), value = T, perl=T)
+ product = sweep(eigenvec, 2, sqrt(eigenval), FUN='*')
+ png(paste0(outpath,basefile,'.pca_loadings.png'))
+ Heatmap(eigenvec[,pc_cols], name = 'PCA loadings 1:20',
+        show_row_names = FALSE, show_column_names = TRUE, 
+        row_dend_reorder = FALSE, column_dend_reorder = FALSE, 
+        cluster_rows = FALSE, cluster_columns = FALSE, column_order = paste0('PC', c(1:20)),
+        use_raster = TRUE, raster_device = 'png', raster_quality = 16)
+ dev.off()
 }
 
 # Biplots of genotype PCA results
@@ -99,13 +75,14 @@ conflevel=0.95 # Confidence level to use when drawing ellipse to estimate sample
 pwidth = 13
 
 # read in the eigenvectors, produced in PLINK
-eigenvec = data.frame(read.table(paste0(basepath,basefile,'.eigenvec'), header=FALSE, skip=0, sep=" "))
+eigenvec = data.frame(read.table(paste0(basepath,basefile,'.eigenvec'), header=FALSE, skip=0, sep=' '))
+eigenval = t(data.frame(read.table(paste0(basepath,basefile,'.eigenval'), header=FALSE, skip=0, sep=' ')))
 rownames(eigenvec) = eigenvec[,2]
 eigenvec = eigenvec[,3:ncol(eigenvec)]
-colnames(eigenvec) = paste0('PC', c(1:20))
+colnames(eigenvec) = colnames(eigenval) = paste0('PC', c(1:20))
 
 # read in the 1kg metadata & read population from 3rd column (col name varies between metadata files I prepared)
-ped = data.frame(read.table(meta1kgfile, header=TRUE, skip=0, sep="\t"))
+ped = data.frame(read.table(meta1kgfile, header=TRUE, skip=0, sep='\t'))
 colnames(ped)[3] = 'ancestry'
 ped = ped[which(ped$IID %in% rownames(eigenvec)), ]
 rownames(ped) = ped$IID
@@ -126,17 +103,14 @@ eigenvec[is.na(eigenvec['ancestry']),'ancestry'] = 'This study'
 
 # Prepare manual color scale
 library(RColorBrewer)
-colscale = brewer.pal(n = length(levels(eigenvec[,'ancestry'])), "Dark2")
+colscale = brewer.pal(n = length(levels(eigenvec[,'ancestry'])), 'Dark2')
 colscale[which(levels(eigenvec[,'ancestry'])=='This study')] = 'black'
-
-#Determine the proportion of variance of each component
-#proportionvariances = ((apply(eigenvec, 1, sd)^2) / (sum(apply(eigenvec, 1, sd)^2)))*100
 
 par(mfrow=c(1,2))
 plot(eigenvec[,1], eigenvec[,2], xlab='PC1', ylab='PC2')
 plot(eigenvec[,1], eigenvec[,3], xlab='PC1', ylab='PC3')
 
-legend("topleft", bty="n", cex=1.5, title="", c("African","Hispanic","East Asian","Caucasian","South Asian"), fill=c("yellow","forestgreen","grey","royalblue","black"))
+legend('topleft', bty='n', cex=1.5, title='', c('African','Hispanic','East Asian','Caucasian','South Asian'), fill=c('yellow','forestgreen','grey','royalblue','black'))
 
 # ggplot it
 library(ggplot2)
@@ -145,29 +119,43 @@ library(sp) # Used to extract plot parameters when reading which points are insi
 npcs = 4 # n PCs to visualize. All pairs 1:n will be plotted
 PCs = seq(1,4,1)
 
-lapply(PCs,function(x){
+pc_pairs = lapply(PCs,function(x){
   lapply(PCs, function(y,x){
     if(x!=y){
       g=ggbiplot(eigenvec,x,y,outpath)
       if(x==2 & y==1){
         annot_eigenvec=est_ancestry(eigenvec,g,x,y,conflevel,outpath)
         write.table(annot_eigenvec,paste0(outpath,basefile,'.eigenvec_ancestry_est'),sep='\t',quote=F)
+        return(annot_eigenvec)
       }
     }
   },x)
 })
 
+# Wrangle ancestry-annotated eigenvector to produce a single column listing all the the 1kg superpopulations 
+# NOTE: solution below does not preserve multiple assigned ancestries if there is an overlap between 2 ellipses--instead only the first is retained in the results. e.g. 'CMC_PITT_178'
+library(dplyr)
 
+annot_eigenvec = pc_pairs[[2]][[1]] 
 est_cols = grep('^est_[.]*', colnames(annot_eigenvec), value = T, perl=T)
-anc = lapply(est_cols,function(x){
-  annot_x = annot_eigenvec[which(annot_eigenvec[,x]),]
-  tryCatch(
-    {
-      annot_x[,'est'] = gsub('est_','',x,perl=T)
-      return(annot_x)
-    },error = function(e) {
-      return(data.frame(colnames=colnames(annot_eigenvec))) 
-    }
-  )
-  #annot_eigenvec[x]=replace(annot_eigenvec[x],T,gsub('est_','',x,perl=T))
-})
+anc = do.call(rbind,
+    lapply(est_cols,function(x){
+    annot_x = annot_eigenvec[which(annot_eigenvec[,x]),]
+    tryCatch(
+      {
+        annot_x['ethnicity_est'] = gsub('est_','',x,perl=T)
+        return(annot_x)
+      },error = function(e) {
+        return(annot_eigenvec[FALSE,]) 
+      }
+    )
+  })
+)
+
+anc$ID = row.names(anc)
+anc_est = anc %>% group_by(ID) %>% summarize(ancestry_est = paste0(sort(unique(ethnicity_est)),collapse=','))
+
+annot_eigenvec$ID = row.names(annot_eigenvec)
+ancestry_estimated = filter(annot_eigenvec,ancestry=='This study') %>% left_join(anc_est,by='ID')
+
+write.table(ancestry_estimated,paste0(outpath,basefile,'.ancestry_est'),sep='\t',quote=F)
